4dcd2bcc9dff9d410ad29c8d64312c05d5ec21bf
kumagai <kumagai@toshiba-tops.co.jp>
2017-11-08 12:52:52 +0900

    ロジックを引越し

--- SmartLogger/SmartLogger.cpp
+++ SmartLogger/SmartLogger.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include "winsvc.h"
 #include "SmartLogger.h"
 
 #include "ServiceInstall.h"
@@ -11,6 +12,7 @@
 #include "Log/StrArray.h"
 #include "Log/ReportEventData.h"
 #include "Log/SmartLoggerLogging.h"
+#include "Log/SmartLoggerReportEventData.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -22,6 +24,308 @@
 CWinApp theApp;
 
 using namespace std;
+
+/*
+ * グローバル変数の定義
+ */
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+HANDLE g_hEventTerminate;	/**< 終了シグナル受信用				*/
+HANDLE g_hThreadService;	/**< サービスメインスレッド作成用	*/
+DWORD g_dwCurrentState;	/**< 現在のサービス状態	*/
+SERVICE_STATUS_HANDLE g_statusService;			/**< サービスステータス取得用		*/
+
+/**
+ * @brief	SCM から呼び出された場合、SMARTLoggerサービスを開始させる
+ *
+ * SCM 以外からの呼び出しでは失敗する
+ *
+ * @param	なし
+ * @return	処理の成功・失敗
+ * @retval	0	正常に登録成功
+ * @retval	0以外	エラー
+ *            - 1:サービスメイン関数の呼び出しに失敗
+ */
+int DispatchService(void)
+{
+	/*
+	 * 変数定義
+	 */
+	BOOL	fRetFunc;			// 呼び出し関数の戻り値
+
+	// サービステーブルの定義
+	SERVICE_TABLE_ENTRY	service_table[] = {
+		{SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain},
+		{NULL, NULL}
+	};
+
+
+	/*
+	 * 変数初期化
+	 */
+
+	g_hEventTerminate	= NULL;
+	g_hThreadService	= NULL;
+
+
+	/*
+	 * サービスディスパッチの実行
+	 *   サービステーブルに定義した ServiceMain 関数の呼び出しが行われる
+	 */
+	fRetFunc = StartServiceCtrlDispatcher(service_table);
+	if( fRetFunc == FALSE )
+	{
+		// ディスパッチに失敗(サービスから呼び出されない場合は失敗する)
+		return 1;
+	}
+
+	// 正常に処理終了
+	return 0;
+}
+
+/**
+ * @brief	サービスメイン関数
+ * @param	argc		[in] 引数個数
+ * @param	argv		[in] 引数配列
+ * @return	なし
+ */
+VOID WINAPI ServiceMain(DWORD argc,	// 引数個数
+						LPTSTR *argv)	// 引数配列
+{
+	/*
+	 * 変数定義
+	 */
+
+	BOOL	fRetFunc;			// 呼び出し関数の戻り値
+	DWORD	dwIdService;		// サービス本体スレッド ID
+
+	/*
+	 * サービスコントロールハンドラ関数の登録
+	 */
+	g_statusService = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+	if( g_statusService == NULL )
+	{
+		// サービスコントロールハンドラ関数の登録失敗 - 即座に終了
+		// ログ出力
+		CSmartLoggerLogging::PutLog(&CStartServiceErrorEvent(_T("サービスコントロールハンドラ関数の登録")));
+		return;
+	}
+
+	// SCM へ状況通知 -  SERVICE_START_PENDING
+	fRetFunc = SendStatusToSCM(SERVICE_START_PENDING, NO_ERROR, 0, 1, 5000);
+	if( fRetFunc == FALSE )
+	{
+		// 通知に失敗 - 即座に終了
+		return;
+	}
+
+
+	/*
+	 * サービス終了シグナルイベントの生成
+	 */
+	g_hEventTerminate = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if( !g_hEventTerminate )
+	{
+		// 失敗 - 即座に終了
+		// ログ出力
+		CSmartLoggerLogging::PutLog(&CStartServiceErrorEvent(_T("サービス終了シグナルイベントの生成")));
+		return;
+	}
+
+	// SCM へ状況通知 -  SERVICE_START_PENDING
+	fRetFunc = SendStatusToSCM(SERVICE_START_PENDING, NO_ERROR, 0, 2, 1000);
+	if( fRetFunc == FALSE )
+	{
+		// 通知に失敗 - 即座に終了
+		return;
+	}
+
+
+	/*
+	 * サービス本体部分の実行 - サービス本体スレッドを作成
+	 */
+	g_hThreadService = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)smartLogger_server, NULL, NULL, &dwIdService);
+	if( g_hThreadService == NULL )
+	{
+		// スレッド作成に失敗 - 即座に終了
+		// ログ出力
+		CSmartLoggerLogging::PutLog(&CStartServiceErrorEvent(_T("サービス本体スレッドの生成")));
+		return;
+	}
+
+	// SCM へ状況通知 -  SERVICE_RUNNING
+	fRetFunc = SendStatusToSCM(SERVICE_RUNNING, NO_ERROR, 0, 0, 0);
+	if( fRetFunc == FALSE )
+	{
+		// 通知に失敗 - 即座に終了
+		if( g_hThreadService )
+		{
+			CloseHandle(g_hEventTerminate);
+		}
+		return;
+	}
+
+	/*
+	 * 終了シグナルの待ち受け
+	 */
+	WaitForSingleObject(g_hEventTerminate, INFINITE);
+
+	// 終了シグナルを検知した場合、シグナル受信用ハンドルをクローズして終了
+	if( g_hEventTerminate )
+	{
+		CloseHandle(g_hEventTerminate);
+	}
+
+	// SCM へ SERVICE_STOPPED を通知する。
+	// ServiceCtrlHandler ではなく、終了シグナルを受け取った時に通知する
+	SendStatusToSCM(SERVICE_STOPPED, NO_ERROR, 0, 0, 100);
+
+	return;
+}
+
+/**
+ * @brief	サービスコントロールハンドラ
+ * @param	fdwControl	[in] 要求したい制御コード
+ * @return	なし
+ */
+VOID WINAPI ServiceCtrlHandler(DWORD fdwControl		// 要求したい制御コード
+							   )
+{
+	/*
+	 * 変数定義
+	 */
+	BOOL	fRetFunc;			// 呼び出し関数の戻り値
+
+	/*
+	 * サービスコントロールコード受信時の各処理定義部
+	 */
+	switch( fdwControl )
+	{
+	/*
+	 * サービス停止要求受信
+	 *   （停止時＝シャットダウン時）
+	 */
+	case SERVICE_CONTROL_STOP:
+	case SERVICE_CONTROL_SHUTDOWN:
+
+		/*
+		 * SCM へ状況通知 -  SERVICE_STOP_PENDING
+		 */
+		fRetFunc = SendStatusToSCM(SERVICE_STOP_PENDING, NO_ERROR, 0, 1, 100);
+		if( fRetFunc == FALSE )
+		{
+			// 通知に成功しなかった場合は特に何もしない
+			break;
+		}
+
+		/*
+		 * サービス本体スレッドをクローズ
+		 */
+		if( g_hThreadService )
+		{
+			CloseHandle(g_hThreadService);
+			g_hThreadService = NULL;
+		}
+
+		// SMARTLOGGERが停止したことを示すログを出力
+		CSmartLoggerLogging::PutLog(&CStopServiceEvent());
+
+		/*
+		 * 終了シグナルの通知
+		 */
+		SetEvent(g_hEventTerminate);
+		return;
+
+	/*
+	 * 状態の更新
+	 */
+	case SERVICE_CONTROL_INTERROGATE:
+		break;
+
+	default:
+		break;
+	}
+
+
+	/*
+	 * SCM へ状況を通知
+	 *   このプログラムでは、実質 SERVICE_CONTROL_INTERROGATE ( + default ) のためだけにある
+	 *   通知失敗時も特に処理を行わない
+	 */
+	fRetFunc = SendStatusToSCM(g_dwCurrentState, NO_ERROR, 0, 0, 0);
+	return;
+}
+
+
+/**
+ * @brief	サービス状況を SCM に通知する
+ * @param	dwCurrentState				[in] サービスステータス
+ * @param	dwWin32ExitCode				[in] エラーコード
+ * @param	dwServiceSpecificExitCode	[in] サービス定義エラーコード
+ * @param	dwCheckPoint				[in] サービス進捗指標
+ * @param	dwWaitHint					[in] 待機時間(ms)
+ * @return	処理の成功・失敗
+ * @retval	TRUE	処理成功
+ * @retval	FALSE	処理に失敗
+ */
+BOOL SendStatusToSCM(DWORD	dwCurrentState,				// サービスステータス
+					 DWORD	dwWin32ExitCode,			// エラーコード
+					 DWORD	dwServiceSpecificExitCode,	// サービス定義エラーコード
+					 DWORD	dwCheckPoint,				// サービス進捗指標
+					 DWORD	dwWaitHint					// 待機時間(ms)
+					 )
+{
+	/*
+	 * 変数定義
+	 */
+	SERVICE_STATUS	status;		// サービスステータス構造体
+	BOOL			fRetFunc;	// 呼び出し関数の戻り値
+
+	/*
+	 * 変数初期化
+	 */
+	status.dwServiceType	= SERVICE_WIN32_OWN_PROCESS;
+	status.dwCurrentState	= dwCurrentState;
+	status.dwCheckPoint		= dwCheckPoint;
+	status.dwWaitHint		= dwWaitHint;
+
+	/*
+	 * サービス状況により、dwControlsAccepted を設定する
+	 *
+	 *   ・SERVICE_START_PENDING(何かの処理を実行中)の場合、
+	 *       コントロールイベントは受け取らない
+	 *   ・それ以外
+	 *       サービス停止(+サービス開始)のみ通知を受け取る
+	 */
+	status.dwControlsAccepted = (dwCurrentState == SERVICE_START_PENDING) ?
+		0 : (SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN);
+
+	/*
+	 * サービス定義エラーを指定された場合はそれを利用する
+	 */
+	status.dwWin32ExitCode = (dwServiceSpecificExitCode == 0) ?
+		dwWin32ExitCode : ERROR_SERVICE_SPECIFIC_ERROR;
+
+
+	/*
+	 * 状態レコードを SCM へ渡す
+	 */
+	fRetFunc = SetServiceStatus(g_statusService, &status);
+	if( fRetFunc == FALSE )
+	{
+		// サービス状態の設定に失敗
+		// ログを出力
+		CSmartLoggerLogging::PutLog(&CSendStatusToSCMErrorEvent(dwCurrentState, dwCheckPoint));
+		return FALSE;
+	}
+
+	g_dwCurrentState = dwCurrentState;
+
+	/*
+	 * 正常終了
+	 */
+	return TRUE;
+}
+
 
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
